a054cb46059d86a7ab9791fd50620f93df1532ff
Prachi Patil <94395731+prachipatil13@users.noreply.github.com>
2023-09-27 16:32:08 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (23.05%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,19 +11,27 @@
  */
 class Solution {
 public:
-    
-    void inOrderhelper(TreeNode* root,vector<int>&ans){
-        if(root==NULL){
-            return;
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack <TreeNode*> st;
+        vector<int> inorder;
+        TreeNode* node=root;
+        
+        while(true){
+            if(node != NULL){
+                st.push(node);
+                node=node->left;
+            }else{
+                if(st.empty()){
+                    break;
+                }else{
+                    node= st.top();
+                    st.pop();
+                    inorder.push_back(node->val);
+                    node=node->right;
+                }
+            }
         }
         
-        inOrderhelper(root->left,ans);
-        ans.push_back(root->val);
-        inOrderhelper(root->right,ans);
-    }
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector <int> ans;
-        inOrderhelper(root,ans);
-        return ans;
+        return inorder;
     }
 };
