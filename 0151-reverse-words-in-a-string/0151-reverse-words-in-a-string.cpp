ddc6fb800947d2a7a451346402dbf502c758db46
Prachi Patil <94395731+prachipatil13@users.noreply.github.com>
2023-11-05 18:18:39 +0530

    Time: 7 ms (43.65%), Space: 21.4 MB (12.33%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string temp="";
+        string current="";
         string ans="";
+        int n=s.size();
+        //removing leading spaces
+        int i=0;
+      while(i<n){
+          while(s[i]== ' '&&i<n){
+               
+              i++;
+           }
+          
+          while(s[i]!=' '&&i<n){
+              current +=s[i];
+              i++;
+          }
+          if(current!=""){
+          if(ans==""){
+              ans=current;
+          }else{
+              ans=current +" "+ans;
+          }
+          current="";
+          }
+      }
         
-            
-            
-            int i=0;
-            int n=s.size();
-        
-        while(i<n){
-            while(s[i]==' '&&i<n){
-                i++;
-            }
-            
-            
-            while(s[i]!=' '&&i<n){
-                temp +=s[i];
-                i++;
-            }
-            
-            if(temp.size()>0){
-                if(ans.size()==0){
-                    ans =temp;
-                }else{
-                    ans= temp + ' '+ans;
-                }
-                temp="";
-            }
-        }
         return ans;
     }
 };
