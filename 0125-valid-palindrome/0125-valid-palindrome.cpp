3fea58c6aa8e3e674c12b43e9a4005d14690a2ce
Prachi Patil <94395731+prachipatil13@users.noreply.github.com>
2023-12-12 23:40:58 +0530

    Time: 6 ms (44.85%), Space: 7.9 MB (37.92%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,8 +1,19 @@
 class Solution {
-private:
-     
+public:
     
-    char lowercase(char ch){
+    bool palindrome(int s,int e,string &st){
+        if(s>=e){
+            return true;
+        }
+        
+        if(st[s]!=st[e]){
+            return false;
+        }
+        
+        return palindrome(s+1,e-1,st);
+    }
+    
+     char lowercase(char ch){
         if((ch>='a'&& ch<='z')||(ch>='0'&& ch<='9')){
             return ch;
         }else{
@@ -10,9 +21,8 @@ private:
             return temp;
         }
     }
-public:
+    
     bool isPalindrome(string s) {
-        //removing non alphanumeric characters
         string temp="";
         
         for(int i=0;i<s.size();i++){
@@ -21,23 +31,9 @@ public:
              char t=lowercase(ch);
                temp +=t;
          }
-        }
-        
-      
-         
-        int i=0;
-        int j=temp.size()-1;
-        
-        while(i<=j){
-            if(temp[i]!=temp[j]){
-                return false;
-            }else{
-                i++;
-                j--;
-            }
-        }
-        
-        return true;
-        
+            
+            
+    }
+        return palindrome(0,temp.length()-1,temp);
     }
 };
