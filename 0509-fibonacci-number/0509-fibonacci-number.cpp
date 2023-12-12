6b89cb2f84f0ee18216212dbc1c0ddabc8cc9971
Prachi Patil <94395731+prachipatil13@users.noreply.github.com>
2023-12-13 00:05:18 +0530

    Time: 13 ms (24.73%), Space: 6.4 MB (46.85%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    
+    int fibonacci(int x){
+        if(x<=1){
+            return x;
+        }
+        
+        return fibonacci(x-1)+fibonacci(x-2);
+    }
+    
+    int fib(int n) {
+        
+  int ans=  fibonacci(n);
+        return ans;
+    }
+};
