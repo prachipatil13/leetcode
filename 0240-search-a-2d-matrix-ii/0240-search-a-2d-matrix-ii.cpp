2148cce54164962c3e3e403dce2b6611cd4ff3fd
Prachi Patil <94395731+prachipatil13@users.noreply.github.com>
2023-12-17 23:25:59 +0530

    Time: 60 ms (93.55%), Space: 15.5 MB (16.86%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row=matrix.size();
+        int col=matrix[0].size();
+        
+        int rowIndex=0;
+        int colIndex=col-1;
+        
+        while(rowIndex<row && colIndex>=0){
+            int ele=matrix[rowIndex][colIndex];
+            
+            if(ele==target){
+                return true;
+            }else if(target<ele){
+                colIndex--;
+            }else{
+                rowIndex++;
+            }
+        }
+        
+        return false;
+    }
+};
