57f832a8025a7d27e6d3eeaf398faa246789c3ed
Prachi Patil <94395731+prachipatil13@users.noreply.github.com>
2023-12-30 21:29:05 +0530

    Time: 3 ms (91.65%), Space: 7.5 MB (29.86%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        vector<int>s1(256,0);
-        vector<int>s2(256,0);
+        vector<int>s1(256,-1);
+        vector<int>s2(256,-1);
         
         if(s.size()!=t.size()){
             return false; 
@@ -10,7 +10,7 @@ public:
         
         for(int i=0;i<s.size();i++){
             if(s1[s[i]]==s2[t[i]]){
-                s1[s[i]]=s2[t[i]]=i+1;
+                s1[s[i]]=s2[t[i]]=i;
             }else{
                 return false;
             }
