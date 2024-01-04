f307bcba5df5c1efcd6f6c1f3f056bd05be81ef8
Prachi Patil <94395731+prachipatil13@users.noreply.github.com>
2024-01-04 21:13:47 +0530

    Time: 3 ms (86.51%), Space: 15.1 MB (82.41%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,47 +11,41 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        
-         if (list1 == NULL) return list2;
+        if (list1 == NULL) return list2;
         if (list2 == NULL) return list1;
 
-        ListNode* newhead=NULL;
-        ListNode * temp;
-        
-        while(list1 !=NULL && list2 !=NULL){
-            if(list1->val <= list2->val){
-                if(newhead==NULL){
-                    newhead=list1;
-                    temp=newhead;
-                    list1=list1->next;
-                }else{
-                ListNode*append=list1;
-                list1=list1->next;
-                temp->next=append;
-                    temp=temp->next;
-                }
-            }else{
-                if(newhead==NULL){
-                    newhead=list2;
-                    temp=newhead;
-                    list2=list2->next;
-                }else{
-               ListNode*append=list2;
-                list2=list2->next;
-                temp->next=append; 
-                     temp=temp->next;
-                }
+        ListNode* newHead = NULL;
+        ListNode* temp = NULL;
+
+        if (list1->val <= list2->val) {
+            newHead = list1;
+            list1 = list1->next;
+        } else {
+            newHead = list2;
+            list2 = list2->next;
+        }
+
+        temp = newHead;
+
+        while (list1 != NULL && list2 != NULL) {
+            if (list1->val <= list2->val) {
+                temp->next = list1;
+                list1 = list1->next;
+            } else {
+                temp->next = list2;
+                list2 = list2->next;
             }
+            temp = temp->next;
         }
-        
-        if(list1!=NULL){
-            temp->next=list1;
+
+        if (list1 != NULL) {
+            temp->next = list1;
         }
-        
-        if(list2!=NULL){
-            temp->next=list2;
+
+        if (list2 != NULL) {
+            temp->next = list2;
         }
-        
-        return newhead;
+
+        return newHead;
     }
 };
