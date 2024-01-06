514ee8f47b4510f47706d47c5d746b4fbb7ceafe
Prachi Patil <94395731+prachipatil13@users.noreply.github.com>
2024-01-06 09:56:38 +0530

    Time: 139 ms (60.78%), Space: 74.3 MB (47.41%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if(head == NULL || head ->next == NULL)
+            return head;
+        
+        //middle of linked list
+        ListNode *temp = NULL;
+        ListNode *slow = head;
+        ListNode *fast = head;
+        
+        while(fast !=  NULL && fast -> next != NULL){
+            temp = slow;
+            slow = slow->next;          
+            fast = fast ->next ->next;  
+            
+        }   
+        temp -> next = NULL;            
+        
+        ListNode* l1 = sortList(head);  //2   
+        ListNode* l2 = sortList(slow);  //1 
+        
+        return mergeTwoLists(l1, l2);         
+            
+    }
+    
+
+    ListNode* mergeTwoLists(ListNode* left, ListNode* right) {
+        
+        if(left == NULL)  return right;
+        if(right == NULL) return left;
+
+        ListNode *ans = new ListNode(-1); 
+        ListNode *mptr = ans;
+        
+    while(left!=NULL && right!=NULL){
+            if(left->val<=right->val){
+                mptr->next = left;
+                mptr = left;
+                left = left->next;
+            }
+            else{
+                mptr->next = right;
+                mptr = right;
+                right = right->next;
+            }
+        }
+    
+    if(left!=NULL){
+        mptr->next=left;
+    }
+    if(right!=NULL){
+        mptr->next=right;
+    }
+    
+    return ans->next;
+    }
+};
