6e3e61e0ecb78ac1c611995bce87988d16025423
Prachi Patil <94395731+prachipatil13@users.noreply.github.com>
2024-01-09 18:29:55 +0530

    Time: 5 ms (18.13%), Space: 8.3 MB (27.27%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        map<int,int>mpp;
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]]++;
+        }
+        int sum=0;
+        for(auto itr=mpp.begin();itr!=mpp.end();itr++){
+            if(itr->second <2){
+                sum += itr->first;
+            }
+        }
+        
+        return sum;
+    }
+};
